bd8e7f1f92f79308e826ab8f11e64c1ab084c9bd
minseok Kim <everybetter@naver.com>
2024-06-05 01:02:16 +0900

    코드 분리 완료

--- js/login.js
+++ js/login.js
@@ -92,26 +92,26 @@ const check_xss = (input) => {
 
                 
                 }
-                function setCookie(name, value, expiredays) {
-                    var date = new Date();
-                    date.setDate(date.getDate() + expiredays);
-                    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + ";path=/" + ";SameSite=None; Secure";
+                // function setCookie(name, value, expiredays) {
+                //     var date = new Date();
+                //     date.setDate(date.getDate() + expiredays);
+                //     document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + ";path=/" + ";SameSite=None; Secure";
                  
-                 }
-                 function getCookie(name) {
-                    var cookie = document.cookie;
-                    console.log("쿠키를 요청합니다.");
-                    if (cookie != "") {
-                    var cookie_array = cookie.split("; ");
-                    for ( var index in cookie_array) {
-                    var cookie_name = cookie_array[index].split("=");
-                    if (cookie_name[0] == "id") {
-                    return cookie_name[1];
-                    }
-                    }
-                    }
-                    return ;
-                    }
+                //  }
+                //  function getCookie(name) {
+                //     var cookie = document.cookie;
+                //     console.log("쿠키를 요청합니다.");
+                //     if (cookie != "") {
+                //     var cookie_array = cookie.split("; ");
+                //     for ( var index in cookie_array) {
+                //     var cookie_name = cookie_array[index].split("=");
+                //     if (cookie_name[0] == "id") {
+                //     return cookie_name[1];
+                //     }
+                //     }
+                //     }
+                //     return ;
+                //     }
 
                 function session_set() { //세션 저장
 let session_id = document.querySelector("#typeEmailX"); // DOM 트리에서 ID 검색
@@ -126,28 +126,28 @@ if (sessionStorage) {
                         }
                         }
 
-                        function session_get() { //세션 읽기
-                            if (sessionStorage) {
-                             return sessionStorage.getItem("Session_Storage_pass");
-                             } else {
-                             alert("세션 스토리지 지원 x");
-                             }
-                             }
-                             function session_check() { //세션 검사
-                                if (sessionStorage.getItem("Session_Storage_test")) {
-                                 alert("이미 로그인 되었습니다."); 
-                                  location.href="../login/index_login.html"; // 로그인된 페이지로 이동
-                                }
-                                 }
+                        // function session_get() { //세션 읽기
+                        //     if (sessionStorage) {
+                        //      return sessionStorage.getItem("Session_Storage_pass");
+                        //      } else {
+                        //      alert("세션 스토리지 지원 x");
+                        //      }
+                        //      }
+                        //      function session_check() { //세션 검사
+                        //         if (sessionStorage.getItem("Session_Storage_test")) {
+                        //          alert("이미 로그인 되었습니다."); 
+                        //           location.href="../login/index_login.html"; // 로그인된 페이지로 이동
+                        //         }
+                        //          }
 
-                                 function session_del() {//세션 삭제
-                                    if (sessionStorage) {
-                                     sessionStorage.removeItem("Session_Storage_test");
-                                     alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
-                                     } else {
-                                     alert("세션 스토리지 지원 x");
-                                     }
-                                     }
+                        //          function session_del() {//세션 삭제
+                        //             if (sessionStorage) {
+                        //              sessionStorage.removeItem("Session_Storage_test");
+                        //              alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+                        //              } else {
+                        //              alert("세션 스토리지 지원 x");
+                        //              }
+                        //              }
                                      function logout(){
                                         session_del(); // 세션 삭제
                                        location.href='../index.html';
@@ -173,38 +173,38 @@ if (sessionStorage) {
                                                 });
                                                 return cipher.toString(CryptoJS.enc.Utf8);
                                                 }
-                                                function encrypt_text(password){
-                                                    const k = "key"; // 클라이언트 키
-                                                   const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
-                                                    const b = password;
-                                                    const eb = this.encodeByAES256(rk, b);
-                                                    return eb;
-                                                    console.log(eb);
-                                                    }
-                                                    function decrypt_text(){
-                                                        const k = "key"; // 서버의 키
-                                                       const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
-                                                        const eb = session_get();
-                                                        const b = this.decodeByAES256(rk, eb);
-                                                        console.log(b); 
-                                                       }
-                                                       function init_logined(){
-                                                        if(sessionStorage){
-                                                        decrypt_text(); // 복호화 함수
-                                                       }
-                                                        else{
-                                                        alert("세션 스토리지 지원 x");
-                                                        }
-                                                        }
+                                                // function encrypt_text(password){
+                                                //     const k = "key"; // 클라이언트 키
+                                                //    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+                                                //     const b = password;
+                                                //     const eb = this.encodeByAES256(rk, b);
+                                                //     return eb;
+                                                //     console.log(eb);
+                                                //     }
+                                                //     function decrypt_text(){
+                                                //         const k = "key"; // 서버의 키
+                                                //        const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+                                                //         const eb = session_get();
+                                                //         const b = this.decodeByAES256(rk, eb);
+                                                //         console.log(b); 
+                                                //        }
+                                                //        function init_logined(){
+                                                //         if(sessionStorage){
+                                                //         decrypt_text(); // 복호화 함수
+                                                //        }
+                                                //         else{
+                                                //         alert("세션 스토리지 지원 x");
+                                                //         }
+                                                //         }
 
-                                                  function addJavascript(jsname) { // 자바스크립트 외부 연동
+                                                 function addJavascript(jsname) { // 자바스크립트 외부 연동
                                                             var th = document.getElementsByTagName('head')[0];
                                                             var s = document.createElement('script');
                                                             s.setAttribute('type','text/javascript');
                                                             s.setAttribute('src',jsname);
                                                             th.appendChild(s);
                                                         }
-                                                  //      addJavascript('/js/security.js'); // 암복호화 함수
-                                                    //    addJavascript('/js/session.js'); // 세션 함수
-                                                      //  addJavascript('/js/cookie.js'); // 쿠키 함수    
+                                                      addJavascript('/js/security.js'); // 암복호화 함수
+                                                       addJavascript('/js/session.js'); // 세션 함수
+                                                        addJavascript('/js/cookie.js'); // 쿠키 함수    
                                                         //교수님이 중복되서 실행되면 오류날수 있다고 했다. 즉 
