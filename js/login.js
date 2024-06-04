87350d8921e098e3d296e44e304b4b87f150306c
minseok Kim <everybetter@naver.com>
2024-06-05 00:37:56 +0900

    수정완료 로그아웃

--- js/login.js
+++ js/login.js
@@ -116,6 +116,7 @@ const check_xss = (input) => {
                 function session_set() { //세션 저장
 let session_id = document.querySelector("#typeEmailX"); // DOM 트리에서 ID 검색
 let session_pass = document.querySelector("#typePasswordX"); // DOM 트리에서 pass 검색
+//let session_pass = document.getElementById("typePasswordX"); // DOM 트리에서 pass 검색 이거나 같네?
 if (sessionStorage) {
  let en_text = encrypt_text(session_pass.value);
  sessionStorage.setItem("Session_Storage_id", session_id.value);
